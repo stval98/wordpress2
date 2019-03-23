51e62a8f65d3b31b01a541c05797ed01eb532f37
Sophia Saint-Val <stval98@gmail.com>
2019-03-23 19:43:48 -0400

    Initialize Bootstrap theme files

---
+++ wp-content/themes/BootSTheme/header.php
@@ -0,0 +1,21 @@
+<?php echo get_stylesheet_uri()?> 
+<?php wp_head(); ?> 
+<!DOCTYPE html>
+<html>
+	<head>
+		<title><?php wp_title(' | ',true,'right'); ></title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		</head>
+	<body>
+		
+		<div class = "navbar navbar-inverse navbar-static-top">
+			<div class = "container">
+				
+				<a href = "#" class = "navbar-brand">Bootstrap WordPress Theme</a>
+				
+				<button class = "navbar-toggle" data-toggle = "collapse" data-target = ".navHeaderCollapse">
+					<span class = "icon-bar"></span>
+					<span class = "icon-bar"></span>
+					<span class = "icon-bar"></span>
+				</button>
+				
