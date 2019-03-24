817fbe1a1b284e0614270a24f8d9291828567a82
Sophia Saint-Val <stval98@gmail.com>
2019-03-23 20:52:04 -0400

    Initialize Bootstrap theme files for Boostrap 4

--- wp-content/themes/BootSTheme/header.php
+++ wp-content/themes/BootSTheme/header.php
@@ -1,21 +1,84 @@
-<?php echo get_stylesheet_uri()?> 
-<?php wp_head(); ?> 
 <!DOCTYPE html>
 <html>
-	<head>
-		<title><?php wp_title(' | ',true,'right'); ></title>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		</head>
-	<body>
-		
-		<div class = "navbar navbar-inverse navbar-static-top">
-			<div class = "container">
-				
-				<a href = "#" class = "navbar-brand">Bootstrap WordPress Theme</a>
-				
-				<button class = "navbar-toggle" data-toggle = "collapse" data-target = ".navHeaderCollapse">
-					<span class = "icon-bar"></span>
-					<span class = "icon-bar"></span>
-					<span class = "icon-bar"></span>
-				</button>
-				
+<head>
+    <title><?php wp_title(' | ',true,'right'); ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+</head>
+<body>
+
+<div class = "navbar navbar-inverse navbar-static-top">
+    <div class = "container">
+
+        <a href = "#" class = "navbar-brand">Bootstrap WordPress Theme</a>
+
+        <button class = "navbar-toggle" data-toggle = "collapse" data-target = ".navHeaderCollapse">
+            <span class = "icon-bar"></span>
+            <span class = "icon-bar"></span>
+            <span class = "icon-bar"></span>
+        </button>
+        <div class = "collapse navbar-collapse navHeaderCollapse">
+
+            <ul class = "nav navbar-nav navbar-right">
+
+                <li><a href = "#">Home</a></li>
+                <li class = "dropdown">
+
+                    <a href = "#" class = "dropdown-toggle" data-toggle = "dropdown">Blog <b class = "caret"></b></a>
+                    <ul class = "dropdown-menu">
+                        <li><a href = "#">Item One</a></li>
+                        <li><a href = "#">Item Two</a></li>
+                        <li><a href = "#">Item Three</a></li>
+                        <li><a href = "#">Item Four</a></li>
+                    </ul>
+
+                </li>
+                <li class = "dropdown">
+
+                    <a href = "#" class = "dropdown-toggle" data-toggle = "dropdown">Social Media <b class = "caret"></b></a>
+                    <ul class = "dropdown-menu">
+                        <li><a href = "#">Twitter</a></li>
+                        <li><a href = "#">Facebook</a></li>
+                        <li><a href = "#">Google+</a></li>
+                        <li><a href = "#">Instagram</a></li>
+                    </ul>
+
+                </li>
+                <li><a href = "#">About</a></li>
+                <li><a href = "#contact" data-toggle="modal">Contact</a></li>
+
+            </ul>
+
+        </div>
+
+    </div>
+</div>
+
+<div class = "container">
+
+    <div class = "jumbotron">
+        <h2>Post Title</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc arcu erat, congue eget ornare et, luctus eget purus. Etiam et risus metus. Nam sed mi tellus. Mauris molestie massa eu metus tempor, in hendrerit arcu adipiscing.</p>
+    </div>
+
+    <div class = "panel panel-default panel-body">
+        <div class = "row">
+            <div class = "col-md-2">
+                <ul class="nav nav-pills nav-stacked">
+                    <li><a href = "#">Category One</a></li>
+                    <li><a href = "#">Category Two</a></li>
+                    <li><a href = "#">Category Three</a></li>
+                    <li><a href = "#">Category Four</a></li>
+                </ul>
+            </div>
+            <div class = "col-md-10">
+                <a href = "#"><h3>This random post is really awesome!</h3></a>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc arcu erat, congue eget ornare et, luctus eget purus. Etiam et risus metus. Nam sed mi tellus. Mauris molestie massa eu metus tempor, in hendrerit arcu adipiscing. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p class = "text-muted">Posted by Ahsan on January 1st 2014</p>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+<?php wp_head(); ?>
--- wp-content/themes/BootSTheme/index.php
+++ wp-content/themes/BootSTheme/index.php
@@ -3,26 +3,26 @@
 <?php
 query_posts('posts_per_page=1');
 while(have_posts()) : the_post(); ?>
-<div>
-<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-<p><?php the_excerpt(); ?></p>
-</div>
+    <div>
+        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        <p><?php the_excerpt(); ?></p>
+    </div>
 <?php endwhile; wp_reset_query(); ?>
 
 <div class="panel panel-default panel-body">
-<div>
-<div>
-<ul>
-<?php wp_list_categories('orderby=name&title_li='); ?>
-</ul>
-</div>
-</div>
+    <div>
+        <div>
+            <ul>
+                <?php wp_list_categories('orderby=name&title_li='); ?>
+            </ul>
+        </div>
+    </div>
 </div>
 
 <div>
-<?php while(have_posts()) : the_post(); ?>
-<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-<p><?php the_excerpt(); ?></p>
-<p> posted by <?php the_author(); ?>
-<?php endwhile; wp_reset_query(); ?>
+    <?php while(have_posts()) : the_post(); ?>
+    <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+    <p><?php the_excerpt(); ?></p>
+    <p> posted by <?php the_author(); ?>
+        <?php endwhile; wp_reset_query(); ?>
 </div>
